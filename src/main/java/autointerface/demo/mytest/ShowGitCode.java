b1a5771f10a355630df4c67cd6e1d66bf5eebd12
jjllcc1981 <23508712@qq.com>
2022-05-22 22:01:33 +0800

    添加内容

--- src/main/java/autointerface/demo/mytest/ShowGitCode.java
+++ src/main/java/autointerface/demo/mytest/ShowGitCode.java
@@ -3,5 +3,6 @@ package autointerface.demo.mytest;
 public class ShowGitCode {
     public static void main(String[] args) {
         System.out.println("查看git中显示，拉分支合并分支");
+        System.out.println("master上提交");
     }
 }
